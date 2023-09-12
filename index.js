67f7f3f1154601509d06574fd3d48e9e1ad127b8
brunoricciweb <bruno.ricci@bue.edu.ar>
2023-09-12 22:38:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.use(express.json());
 var cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
+var fakeDB = {
+    'bruno_web': {
+        productos: []
+    },
+    'ariel123':{
+        productos: []
+    }
+}
 
 app.get('/', (req, res) => { // devolver todos los productos
     console.log('entró a GET /')
@@ -13,8 +21,8 @@ app.get('/', (req, res) => { // devolver todos los productos
 })
 
 app.get('/set_cookie',function(req, res){
-    var cookie_name = 'soidjasdoissa'
-    res.cookie(cookie_name , '132131').send('Cookie is set');
+    var cookie_name = 'username'
+    res.cookie(cookie_name , 'ariel123').send('Cookie is set');
 });
 
 app.get('/read_cookie',function(req, res){
@@ -23,6 +31,18 @@ app.get('/read_cookie',function(req, res){
 });
 
 
+
+function middlewareAutenticacion(req, res, next){
+    console.log('Este es el middleware!');
+    console.log('Usuario en la cookie:', req.cookies.username);
+    next();
+}
+
+// endpoint que necesita autenticación
+app.get('/seguro', middlewareAutenticacion ,(req, res, next) => { // devolver todos los productos
+    console.log('entró a GET /seguro')
+    res.send('Endpoint seguro');
+})
 
 /////////////////////////////////////////////
 app.listen(port, () => {
