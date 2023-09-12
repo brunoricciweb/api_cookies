3dc9fccfa71080253e62c8d7e3052668fa658d26
brunoricciweb <bruno.ricci@bue.edu.ar>
2023-09-12 21:54:11 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+app.use(express.json());
+var cookieParser = require('cookie-parser')
+
+
+app.get('/', (req, res) => { // devolver todos los productos
+    console.log('entró a GET /')
+    res.send('funciona ok!');
+})
+
+app.get('/set_cookie',function(req, res){
+    var cookie_name = 'soidjasdoissa'
+    res.cookie(cookie_name , '132131').send('Cookie is set');
+});
+
+app.get('/read_cookie',function(req, res){
+    console.log("Cookies :  ", req.cookies);
+    res.send(req.cookies);
+});
+
+
+
+/////////////////////////////////////////////
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
